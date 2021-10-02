195b5d73a3457a522a1ca51574d7cb2fe58184fa
mayara bueno <luziamayara83@gmail.com>
2021-10-02 14:46:12 -0300

    componente sem consumo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { FormsModule } from '@angular/forms';
 import { InicioComponent } from './inicio/inicio.component';
+import { HashLocationStrategy, LocationStrategy}  from '@angular/common'
+
 
 @NgModule({
   declarations: [
@@ -25,7 +27,11 @@ import { InicioComponent } from './inicio/inicio.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide:LocationStrategy,
+    useClass:HashLocationStrategy
+    
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
